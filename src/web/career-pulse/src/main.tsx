f8ff8fd2a279a6667fd0ff534fe94131cfd25b76
Focus <maniaclord@bk.ru>
2025-01-29 01:35:10 +0300

    CA-34: Настройка стилей. CA-44: Страница для входа. CA-45: Страница для регистрации.

--- src/web/career-pulse/src/main.tsx
+++ src/web/career-pulse/src/main.tsx
@@ -3,11 +3,14 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {ErrorProvider} from "./ErrorContext.tsx";
+import {StyledEngineProvider} from "@mui/material";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <ErrorProvider>
-            <App/>
-        </ErrorProvider>
+        <StyledEngineProvider injectFirst>
+            <ErrorProvider>
+                <App/>
+            </ErrorProvider>
+        </StyledEngineProvider>
     </StrictMode>
 )
--- src/web/career-pulse/src/pages/LoginPage.tsx
+++ src/web/career-pulse/src/pages/LoginPage.tsx
@@ -1,39 +1,143 @@
-import React from "react";
-import {Link, useNavigate} from "react-router-dom";
-import {TextField, Button, Box, Typography, Container} from "@mui/material";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import {AuthCard} from "../components/auth/AuthCard.tsx";
+import {AuthContainer} from "../components/auth/AuthContainer.tsx";
 import {PageRoute} from "../utils/navigation/PageRoute.tsx";
 
-export const LoginPage: React.FC = () => {
-    const navigate = useNavigate();
+export const LoginPage = () => {
+    const [emailError, setEmailError] = React.useState(false);
+    const [emailErrorMessage, setEmailErrorMessage] = React.useState('');
+    const [passwordError, setPasswordError] = React.useState(false);
+    const [passwordErrorMessage, setPasswordErrorMessage] = React.useState('');
 
-    const handleLogin = () => {
-        // Здесь вы можете обработать вход в аккаунт
-        console.log("Logged in!");
-        navigate(PageRoute.main);
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        if (emailError || passwordError) {
+            event.preventDefault();
+            return;
+        }
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    const validateInputs = () => {
+        const email = document.getElementById('email') as HTMLInputElement;
+        const password = document.getElementById('password') as HTMLInputElement;
+
+        let isValid = true;
+
+        if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
+            setEmailError(true);
+            setEmailErrorMessage('Please enter a valid email address.');
+            isValid = false;
+        } else {
+            setEmailError(false);
+            setEmailErrorMessage('');
+        }
+
+        if (!password.value || password.value.length < 6) {
+            setPasswordError(true);
+            setPasswordErrorMessage('Password must be at least 6 characters long.');
+            isValid = false;
+        } else {
+            setPasswordError(false);
+            setPasswordErrorMessage('');
+        }
+
+        return isValid;
     };
 
     return (
-        <Container maxWidth="sm"
-                   style={{display: "flex", flexDirection: "column", alignItems: "center", marginTop: "100px"}}>
-            <Typography variant="h4" gutterBottom>
-                Вход в аккаунт
-            </Typography>
-            <Box component="form" width="100%" mt={2}>
-                <TextField label="Email" variant="outlined" fullWidth margin="normal"/>
-                <TextField label="Пароль" variant="outlined" type="password" fullWidth margin="normal"/>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    fullWidth
-                    style={{marginTop: "16px"}}
-                    onClick={handleLogin}
+        <AuthContainer direction="column" justifyContent="space-between">
+            <AuthCard variant="outlined">
+                <Typography
+                    component="h1"
+                    variant="h4"
+                    sx={{width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)'}}
                 >
-                    Войти
-                </Button>
-            </Box>
-            <Typography variant="body2" style={{marginTop: "16px"}}>
-                Нет аккаунта? <Link to={PageRoute.register}>Зарегистрируйтесь</Link>
-            </Typography>
-        </Container>
+                    Вход
+                </Typography>
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit}
+                    noValidate
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '100%',
+                        gap: 2,
+                    }}
+                >
+                    <FormControl>
+                        <FormLabel htmlFor="email">Email</FormLabel>
+                        <TextField
+                            error={emailError}
+                            helperText={emailErrorMessage}
+                            id="email"
+                            type="email"
+                            name="email"
+                            placeholder="your@email.com"
+                            autoComplete="email"
+                            autoFocus
+                            required
+                            fullWidth
+                            variant="outlined"
+                            color={emailError ? 'error' : 'primary'}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel htmlFor="password">Пароль</FormLabel>
+                        <TextField
+                            error={passwordError}
+                            helperText={passwordErrorMessage}
+                            name="password"
+                            placeholder="••••••"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            autoFocus
+                            required
+                            fullWidth
+                            variant="outlined"
+                            color={passwordError ? 'error' : 'primary'}
+                        />
+                    </FormControl>
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary"/>}
+                        label="Запомнить меня"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        onClick={validateInputs}
+                    >
+                        Войти
+                    </Button>
+                </Box>
+                <Box sx={{display: 'flex', flexDirection: 'column', gap: 2}}>
+                    <Typography sx={{textAlign: 'center'}}>
+                        Нет аккаунта?{' '}
+                        <Link
+                            href={PageRoute.register}
+                            variant="body2"
+                            sx={{alignSelf: 'center'}}
+                        >
+                            Зарегистрироваться
+                        </Link>
+                    </Typography>
+                </Box>
+            </AuthCard>
+        </AuthContainer>
     );
-};
+}
