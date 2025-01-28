5b91b35f18421a0c60bac8d7ce4cdce343b80e3b
Focus <maniaclord@bk.ru>
2025-01-29 00:19:50 +0300

    CA-36: Navigation bar

---
+++ src/web/career-pulse/src/components/NavBar.tsx
@@ -0,0 +1,26 @@
+import {AppBar, Toolbar, Typography, Button, Box} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+import {PageRoute} from "../utils/navigation/PageRoute.tsx";
+
+const NavBar = () => {
+    const navigate = useNavigate();
+
+    return (
+        <AppBar position="static" color="primary">
+            <Toolbar sx={{display: "flex", justifyContent: "space-between"}}>
+                {/* Лого и название */}
+                <Box sx={{display: "flex", alignItems: "center", cursor: "pointer"}} onClick={() => navigate("/")}>
+                    <Typography variant="h6" fontWeight="bold" sx={{mr: 2}}>
+                        CareerPulse
+                    </Typography>
+                </Box>
+
+                <Button color="inherit" onClick={() => navigate(PageRoute.login)}>
+                    Войти
+                </Button>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default NavBar;
