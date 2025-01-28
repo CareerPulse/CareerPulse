c3413c1fa22b4fbb6c9d4444c1fbee0bdf073c30
Focus <maniaclord@bk.ru>
2025-01-29 00:58:16 +0300

    CA-32: Страница продвинутого поиска - фильтры

--- src/web/career-pulse/src/utils/navigation/PageRoute.tsx
+++ src/web/career-pulse/src/utils/navigation/PageRoute.tsx
@@ -1,6 +1,8 @@
 export enum PageRoute {
+    main = "/",
     saved = "/saved",
     searchVacancy = "/search/vacancy",
+    advancedSearchVacancy = "/search/vacancy/advanced",
     register = "/auth/register",
     login = "/auth/login"
 }
